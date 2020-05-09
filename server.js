aaf8288c792874fd3851d4f5b3087154da84bf1d
Ayush Salik <asalik13@gmail.com>
2020-05-09 19:17:03 -0400

    deploy

--- server.js
+++ server.js
@@ -96,9 +96,6 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(require("./routes"));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'Client/build', 'index.html'))
-})
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'Client/build', 'index.html'));
 });
